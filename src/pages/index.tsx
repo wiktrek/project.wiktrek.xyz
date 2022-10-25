beaeb25e34fc2e7069ce926d35fe9e9e4a4286f7
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-10-25 20:54:28 +0200

    test

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>projects - wiktrek.xyz</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+      </main>
+    </>
+  );
+};
+
+export default Home;
