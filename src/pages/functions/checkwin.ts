2628943ca552191b34766d9fecf6b03d74225a34
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-12-30 23:48:19 +0100

    WORKS!

---
+++ src/pages/functions/checkwin.ts
@@ -0,0 +1,36 @@
+function checkwin(
+  one: string,
+  two: string,
+  three: string,
+  four: string,
+  five: string,
+  six: string,
+  seven: string,
+  eight: string,
+  nine: string
+) {
+  let win = "";
+  if (one === "❌" && two === "❌" && three === "❌") return (win = "❌ won");
+  if (four === "❌" && five === "❌" && six === "❌") return (win = "❌ won");
+  if (seven === "❌" && eight === "❌" && nine === "❌")
+    return (win = "❌ won");
+  if (one === "❌" && five === "❌" && nine === "❌") return (win = "❌ won");
+  if (three === "❌" && five === "❌" && seven === "❌")
+    return (win = "❌ won");
+  if (one === "❌" && four === "❌" && seven === "❌") return (win = "❌ won");
+  if (two === "❌" && five === "❌" && eight === "❌") return (win = "❌ won");
+  if (three === "❌" && six === "❌" && nine === "❌") return (win = "❌ won");
+  if (one === "⭕" && two === "⭕" && three === "⭕") return (win = "⭕ won");
+  if (four === "⭕" && five === "⭕" && six === "⭕") return (win = "⭕ won");
+  if (seven === "⭕" && eight === "⭕" && nine === "⭕")
+    return (win = "⭕ won");
+  if (one === "⭕" && five === "⭕" && nine === "⭕") return (win = "⭕ won");
+  if (three === "⭕" && five === "⭕" && seven === "⭕")
+    return (win = "⭕ won");
+  if (one === "⭕" && four === "⭕" && seven === "⭕") return (win = "⭕ won");
+  if (two === "⭕" && five === "⭕" && eight === "⭕") return (win = "⭕ won");
+  if (three === "⭕" && six === "⭕" && nine === "⭕") return (win = "⭕ won");
+
+  return win;
+}
+export default checkwin;
--- src/pages/tictactoe.tsx
+++ src/pages/tictactoe.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
-
+import checkwin from "./functions/checkwin";
 const Home: NextPage = () => {
+  // ⭕
+  const [pick, setpick] = useState("❌");
   const [one, setone] = useState("");
   const [two, settwo] = useState("");
   const [three, setthree] = useState("");
@@ -12,10 +14,67 @@ const Home: NextPage = () => {
   const [seven, setseven] = useState("");
   const [eight, seteight] = useState("");
   const [nine, setnine] = useState("");
+  const [win, setwin] = useState({ text: "", won: false });
   function Click(e: React.MouseEvent<HTMLButtonElement>) {
+    if (win.won === true) return;
     const id = (e.target as HTMLElement).id;
     console.log(id);
+    let a = 0;
+    if (id === "one" && one === "") {
+      a = 1;
+      setone(pick);
+    }
+    if (id === "two" && two === "") {
+      a = 1;
+      settwo(pick);
+    }
+    if (id === "three" && three === "") {
+      a = 1;
+      setthree(pick);
+    }
+    if (id === "four" && four === "") {
+      a = 1;
+      setfour(pick);
+    }
+    if (id === "five" && five === "") {
+      a = 1;
+      setfive(pick);
+    }
+    if (id === "six" && six === "") {
+      a = 1;
+      setsix(pick);
+    }
+    if (id === "seven" && seven === "") {
+      a = 1;
+      setseven(pick);
+    }
+    if (id === "eight" && eight === "") {
+      a = 1;
+      seteight(pick);
+    }
+    if (id === "nine" && nine === "") {
+      a = 1;
+      setnine(pick);
+    }
+
+    if (a === 0) return;
+
+    if (pick === "❌") return setpick("⭕");
+    if (pick === "⭕") return setpick("❌");
   }
+  function check() {
+    clearInterval(interval);
+    if (win.text !== "") {
+      return setwin({ text: win.text, won: true });
+    }
+    setwin({
+      text: `${checkwin(one, two, three, four, five, six, seven, eight, nine)}`,
+      won: false,
+    });
+  }
+
+  let interval = setInterval(check, 400);
+
   return (
     <>
       <Head>
@@ -54,6 +113,7 @@ const Home: NextPage = () => {
             {nine}
           </button>
         </div>
+        <a>{win.text}</a>
       </main>
     </>
   );
