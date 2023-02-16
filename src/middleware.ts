3737bcbfcd3868c9c20500cc5ceb224b8586973b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-16 21:18:32 +0100

    please work

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest, res: NextResponse) {
+  console.log(req.nextUrl.pathname);
+  if (req.nextUrl.pathname.startsWith('/go')) {
+    let a = `https://project.wiktrek.xyz/api${req.nextUrl.pathname}`;
+    return NextResponse.redirect(a);
+  }
+  if (req.nextUrl.pathname.startsWith('/q')) {
+    let a = `https://project.wiktrek.xyz/projects/poll${req.nextUrl.pathname}`;
+    return NextResponse.redirect(a);
+  }
+  if (req.nextUrl.pathname === '/poll') {
+    let a = `https://project.wiktrek.xyz/projects/poll`;
+    return NextResponse.redirect(a);
+  }
+}
