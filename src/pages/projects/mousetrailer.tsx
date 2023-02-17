aa09d4dd3ddf888d90fc3e1aab3261bd1b0f9b10
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-17 22:10:04 +0100

    WORKS

--- src/pages/projects/mousetrailer.tsx
+++ src/pages/projects/mousetrailer.tsx
@@ -9,7 +9,10 @@ const Rng: NextPage = () => {
       window.onmousemove = (e) => {
         const x = e.clientX - trailer.offsetWidth / 2,
           y = e.clientY - trailer.offsetHeight / 2;
-        trailer.style.transform = `translate(${x}px,${y}px)`;
+        const keyframes = {
+          transform: `translate(${x}px,${y}px)`,
+        };
+        trailer.animate(keyframes, { duration: 800, fill: "forwards" });
       };
     }
   }, []);
