d11767f5b7925b5ad88d2f9fbf335e61b3386d5e
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-18 18:08:16 +0100

    url

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar: NextPage = () => {
   return (
     <>
-      <div className="text-1xl flex h-4 list-none items-center justify-between p-7">
+      <div className="flex h-4 list-none items-center justify-between p-7 text-xl">
         <li>
           <a href="https://wiktrek.xyz" target="_blank" rel="noreferrer">
             wiktrek.xyz
--- src/pages/projects/url/index.tsx
+++ src/pages/projects/url/index.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable */
-import { NextApiRequest, NextApiResponse } from 'next';
-import React, { useState } from 'react';
-import { PrismaClient } from '@prisma/client';
-import type { NextPage, GetServerSideProps } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import copy from 'copy-to-clipboard';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { NextApiRequest, NextApiResponse } from "next";
+import React, { useState } from "react";
+import { PrismaClient } from "@prisma/client";
+import type { NextPage, GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import copy from "copy-to-clipboard";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faTrash } from "@fortawesome/free-solid-svg-icons";
 import {
   useUser,
   UserProvider,
   withPageAuthRequired,
   getSession,
-} from '@auth0/nextjs-auth0';
-import Link from 'next/link';
+} from "@auth0/nextjs-auth0";
+import Link from "next/link";
 interface Props {
   link: any;
   alllinks: any;
@@ -23,9 +23,9 @@ interface Props {
 const prisma = new PrismaClient();
 const Home: NextPage<Props> = ({ link }) => {
   const { user, error, isLoading } = useUser();
-  const [slug, setSlug] = useState('');
-  const [url, setUrl] = useState('');
-  const [newerror, setError] = useState('');
+  const [slug, setSlug] = useState("");
+  const [url, setUrl] = useState("");
+  const [newerror, setError] = useState("");
   const CopyUrl = async (event: React.SyntheticEvent) => {
     // @ts-ignore: Object is possibly 'null'.
     const rowId = event.currentTarget.parentNode.id;
@@ -39,11 +39,11 @@ const Home: NextPage<Props> = ({ link }) => {
     // @ts-ignore: Object is possibly 'null'.
     const rowId = event.currentTarget.parentNode.id;
 
-    if (typeof rowId === 'string') {
+    if (typeof rowId === "string") {
       if (rowId === null) return;
       console.log(rowId);
       axios
-        .post('/api/functions/delete', {
+        .post("/api/functions/delete", {
           slug: rowId,
         })
         .then(function (response) {
@@ -58,10 +58,10 @@ const Home: NextPage<Props> = ({ link }) => {
   };
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    if (slug === '' || url === '') return;
+    if (slug === "" || url === "") return;
     console.log(slug, url, user?.name);
     axios
-      .post('/api/functions/save', {
+      .post("/api/functions/save", {
         owner: user?.name,
         url: url,
         slug: slug,
@@ -83,52 +83,56 @@ const Home: NextPage<Props> = ({ link }) => {
         <title>Url shortener- wiktrek</title>
       </Head>
       <UserProvider>
-        <div className="">
-          <h1>create new short url</h1>
-          <Link href="/api/auth/logout">logout</Link>
-          <form onSubmit={submitData}>
-            <input
-              autoFocus
-              onChange={(e) => setSlug(e.target.value)}
-              placeholder="slug"
-              type="text"
-              value={slug}
-            />
-            <input
-              type="url"
-              onChange={(e) => setUrl(e.target.value)}
-              placeholder="url"
-              value={url}
-            />
-            <button type="submit" className="formbutton">
-              Submit
-            </button>
-          </form>
-          <a>{newerror}</a>
-        </div>
-        <div className="{styles.center}">
-          <table className="">
-            <tbody>
-              {link.map((item: any, index: any) => {
-                return (
-                  <>
-                    <div className="" id={item.slug}>
-                      {index + 1 + ' '}
-                      {item.slug}
-                      {/* url: {item.url} */}
-                      <button onClick={CopyUrl}>
-                        <FontAwesomeIcon icon={faClipboard} />
-                      </button>
-                      <button onClick={Deletefunction} data-slug={item.slug}>
-                        <FontAwesomeIcon icon={faTrash} />
-                      </button>
-                    </div>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <main className="mx-auto flex  w-screen flex-col items-center justify-center text-center text-xl text-white">
+          <div>
+            <h1>create new short url</h1>
+            <Link href="/api/auth/logout">logout</Link>
+            <form onSubmit={submitData}>
+              <input
+                className="text-black"
+                autoFocus
+                onChange={(e) => setSlug(e.target.value)}
+                placeholder="slug"
+                type="text"
+                value={slug}
+              />
+              <input
+                className="text-black"
+                type="url"
+                onChange={(e) => setUrl(e.target.value)}
+                placeholder="url"
+                value={url}
+              />
+              <button type="submit" className="formbutton">
+                Submit
+              </button>
+            </form>
+            <a>{newerror}</a>
+          </div>
+          <div className="">
+            <table className="">
+              <tbody>
+                {link.map((item: any, index: any) => {
+                  return (
+                    <>
+                      <div className="" id={item.slug}>
+                        {index + 1 + " "}
+                        {item.slug}
+                        {/* url: {item.url} */}
+                        <button onClick={CopyUrl}>
+                          <FontAwesomeIcon icon={faClipboard} />
+                        </button>
+                        <button onClick={Deletefunction} data-slug={item.slug}>
+                          <FontAwesomeIcon icon={faTrash} />
+                        </button>
+                      </div>
+                    </>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </main>
       </UserProvider>
     </>
   );
