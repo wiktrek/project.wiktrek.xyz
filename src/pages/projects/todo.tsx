7293b36c90116d005a36e78fe1508ab0c9524187
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-22 22:32:34 +0100

    todo.tsx

--- src/pages/projects/todo.tsx
+++ src/pages/projects/todo.tsx
@@ -5,15 +5,21 @@ import { useState } from "react";
 import Todo from "../components/todo";
 
 const Home: NextPage = () => {
+  function removeTodo(e: React.SyntheticEvent) {
+    e.currentTarget.id;
+  }
   return (
     <>
       <Head>
-        <title>Rock paper scissors - wiktrek</title>
+        <title>to do- wiktrek</title>
         <meta name="description" content="Rock paper scissors" />
       </Head>
       <div className="mx-auto flex  w-screen flex-col items-center justify-center text-center text-xl">
         <button>Add a task</button>
-        <Todo name="tidy your room" />
+        <div>
+          <Todo name="tidy your room" />
+          <input id="id" type="checkbox" onClick={removeTodo}></input>
+        </div>
       </div>
     </>
   );
