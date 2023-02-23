26ae0e8286172c73f0510548f5297d8d63fb4a23
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-23 21:14:06 +0100

    todo.tsx

--- src/pages/projects/todo.tsx
+++ src/pages/projects/todo.tsx
@@ -5,11 +5,22 @@ import React, { useEffect, useState } from "react";
 import Todo from "../components/todo";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { v4: uuidv4 } = require("uuid");
+interface Todo {
+  name: string;
+  id: string;
+}
 const Home: NextPage = () => {
-  const [todos, setTodos] = useState([{}]);
+  const [todos, setTodos] = useState([{} as Todo]);
   function removeTodo(e: React.SyntheticEvent) {
-    const name = e.currentTarget.id;
-    todos.push({ name: name });
+    const todoarr = [{} as Todo];
+
+    todos.map((todo) => {
+      if (todo.id === e.currentTarget.id) return;
+      if (todo.name === "") return;
+      todoarr.push(todo);
+    });
+    setTodos(todoarr);
+    // todos.push({ name: name, id: e.currentTarget.id });
     console.log(todos);
   }
   return (
