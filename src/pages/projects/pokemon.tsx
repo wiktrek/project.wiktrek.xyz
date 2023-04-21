e3a9ffbb40b78635a527fa84d4cadea062322b5b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-21 21:19:11 +0200

    pokeapi

--- src/pages/projects/pokemon.tsx
+++ src/pages/projects/pokemon.tsx
@@ -1,8 +1,29 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React, { useState } from "react";
-
+import axios from "axios";
 const Pokemon: NextPage = () => {
+  const [pokemon, setPokemon] = useState("");
+  const [pokemon2, setPokemon2] = useState("");
+  function getRandomPokemon() {
+    axios
+      .get(
+        "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0https://pokeapi.co/api/v2/"
+      )
+      .then(function (response) {
+        // handle success
+
+        console.log(response, response.data.count);
+      });
+  }
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon/snorlax")
+    .then(function (response) {
+      // handle success
+      console.log(response.data.height / 10);
+      setPokemon(response.data.name);
+    });
+
   return (
     <>
       <Head>
@@ -11,7 +32,9 @@ const Pokemon: NextPage = () => {
       </Head>
       <div>
         <button>button 1</button>
+        <p>{pokemon}</p>
         <button>button 2</button>
+        <button onClick={getRandomPokemon}>Get random pokemon</button>
       </div>
     </>
   );
