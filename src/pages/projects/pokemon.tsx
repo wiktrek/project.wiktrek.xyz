f60b9ae509dfd58c5d8752be2b4a663b6ac5c589
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-21 22:02:03 +0200

    pokemon

--- src/pages/projects/pokemon.tsx
+++ src/pages/projects/pokemon.tsx
@@ -2,13 +2,17 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Pokemon: NextPage = () => {
   const [pokemon, setPokemon] = useState("");
   const [pokemon2, setPokemon2] = useState("");
+  const [height, setHeight] = useState(0);
+  const [height2, setHeight2] = useState(0);
   const [image, setImage] = useState("");
   const [image2, setImage2] = useState("");
+  const [anwser, setAnwser] = useState("");
+
   function getRandomPokemon() {
     axios
       .get(
@@ -28,11 +32,22 @@ const Pokemon: NextPage = () => {
         getPokemon(response.data.results[randomnumber2].url, 2);
       });
   }
+  function taller(e: React.SyntheticEvent) {
+    switch (e.currentTarget.id) {
+      case "first": {
+        break;
+      }
+      case "second": {
+      }
+    }
+  }
   function getPokemon(url: string, number: number) {
     switch (number) {
       case 1: {
         axios.get(url).then(function (response) {
+          console.log(response.data);
           setPokemon(response.data.name);
+          setHeight(response.data.height / 10);
           setImage(
             `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` +
               response.data.id +
@@ -43,7 +58,9 @@ const Pokemon: NextPage = () => {
       }
       case 2: {
         axios.get(url).then(function (response) {
+          console.log(response.data);
           setPokemon2(response.data.name);
+          setHeight2(response.data.height / 10);
           setImage2(
             `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` +
               response.data.id +
@@ -54,22 +71,25 @@ const Pokemon: NextPage = () => {
       }
     }
   }
+  useEffect(() => {
+    getRandomPokemon();
+  }, []);
   return (
     <>
       <Head>
         <title>Pokemon - wiktrek</title>
         <meta name="description" content="Random number generator" />
       </Head>
-      <div className="text-center">
+      <div className="text-center text-2xl">
         <a>Select which pokemon is taller</a>
         <div className=" flex items-center justify-center text-center">
           <div>
             <img src={image} alt="image" width={200} height={200} />
-            <button>{pokemon}</button>
+            <button id="first">{pokemon}</button>
           </div>
           <div>
             <img src={image2} alt="image2" width={200} height={200} />
-            <button>{pokemon2}</button>
+            <button id="second">{pokemon2}</button>
           </div>
         </div>
 
