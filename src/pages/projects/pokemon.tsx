cf682917b476ded6f0a47ad7f83c5f577bc50dfe
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-21 22:16:15 +0200

    pokemon finished!

--- src/pages/projects/pokemon.tsx
+++ src/pages/projects/pokemon.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
-import Image from "next/image";
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -14,6 +13,7 @@ const Pokemon: NextPage = () => {
   const [anwser, setAnwser] = useState("");
 
   function getRandomPokemon() {
+    setAnwser("");
     axios
       .get(
         "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0https://pokeapi.co/api/v2/"
@@ -25,19 +25,47 @@ const Pokemon: NextPage = () => {
           Math.floor(Math.random() * 1000) + (response.data.count - 1000);
         const randomnumber2 =
           Math.floor(Math.random() * 1000) + (response.data.count - 1000);
-        console.log(response, response.data.count);
-        console.log(response.data.results[randomnumber].url);
+        // console.log(response, response.data.count);
+        // console.log(response.data.results[randomnumber].url);
 
         getPokemon(response.data.results[randomnumber].url, 1);
         getPokemon(response.data.results[randomnumber2].url, 2);
       });
   }
   function taller(e: React.SyntheticEvent) {
+    if (height == height2) {
+      getRandomPokemon();
+    }
     switch (e.currentTarget.id) {
       case "first": {
+        if (height > height2) {
+          setAnwser(
+            `Your anwser is correct! ${pokemon} is taller than ${pokemon2}`
+          );
+          getRandomPokemon();
+        }
+        if (height < height2) {
+          setAnwser(
+            `Your anwser is incorrect! ${pokemon} is shorter than ${pokemon2}`
+          );
+          getRandomPokemon();
+        }
         break;
       }
       case "second": {
+        if (height2 > height) {
+          setAnwser(
+            `Your anwser is correct! ${pokemon2} is taller than ${pokemon}`
+          );
+          getRandomPokemon();
+        }
+        if (height > height2) {
+          setAnwser(
+            `Your anwser is incorrect! ${pokemon2} is shorter than ${pokemon}`
+          );
+          getRandomPokemon();
+        }
+        break;
       }
     }
   }
@@ -45,8 +73,7 @@ const Pokemon: NextPage = () => {
     switch (number) {
       case 1: {
         axios.get(url).then(function (response) {
-          console.log(response.data);
-          setPokemon(response.data.name);
+          setPokemon(response.data.species.name);
           setHeight(response.data.height / 10);
           setImage(
             `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` +
@@ -58,8 +85,7 @@ const Pokemon: NextPage = () => {
       }
       case 2: {
         axios.get(url).then(function (response) {
-          console.log(response.data);
-          setPokemon2(response.data.name);
+          setPokemon2(response.data.species.name);
           setHeight2(response.data.height / 10);
           setImage2(
             `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` +
@@ -73,6 +99,7 @@ const Pokemon: NextPage = () => {
   }
   useEffect(() => {
     getRandomPokemon();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
@@ -85,14 +112,18 @@ const Pokemon: NextPage = () => {
         <div className=" flex items-center justify-center text-center">
           <div>
             <img src={image} alt="image" width={200} height={200} />
-            <button id="first">{pokemon}</button>
+            <button id="first" onClick={taller}>
+              {pokemon}
+            </button>
           </div>
           <div>
             <img src={image2} alt="image2" width={200} height={200} />
-            <button id="second">{pokemon2}</button>
+            <button id="second" onClick={taller}>
+              {pokemon2}
+            </button>
           </div>
         </div>
-
+        <p>{anwser}</p>
         <button onClick={getRandomPokemon}>Get random pokemon</button>
       </div>
     </>
