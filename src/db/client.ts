c363b6adc4668bc351615049a374f9dbc82d9db9
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-05 00:08:52 +0200

    schema

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
- import { pollQuestion, shortLink, vote} from '../../drizzle/schema'
+ import { pollQuestion, shortLink, vote} from './schema'
 // create the connection
 const connection = connect({
  url: process.env.DATABASE_URL,
