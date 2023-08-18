fbe6ce85684098a4aa206a837d4e4b72333b2170
wiktrek <wiktor.janiszewski10@gmail.com>
2023-08-18 18:22:57 +0200

    clerk + msg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
+
 const Navbar: NextPage = () => {
   return (
     <>
