e0984c2383b61cdff49bc621ce7ecf97b697d68c
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-10 17:42:37 +0200

    middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { withClerkMiddleware } from "@clerk/nextjs";
+const projects = [
+  "term",
+  "poll",
+  "url"
+]
 function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/q")) {
     const a = `https://project.wiktrek.xyz/projects/poll${req.nextUrl.pathname}`;
     return NextResponse.redirect(a);
   }
-  if (req.nextUrl.pathname === "/poll") {
-    const a = `https://project.wiktrek.xyz/projects/poll`;
-    return NextResponse.redirect(a);
+
+   for (let i = 0; i < projects.length; i++) {
+    if (req.nextUrl.pathname === '/' + projects[i]) {
+  const a = `https://project.wiktrek.xyz/projects/${projects[i]}`;
+      return NextResponse.redirect(a);
+    }
   }
 }
-import { withClerkMiddleware } from "@clerk/nextjs";
  
 
 // export default authMiddleware({
