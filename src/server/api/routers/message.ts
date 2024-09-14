dc2b08b2fb8d96dfb4b4ced97efa9157fba53bc8
wiktor <wiktor.janiszewski10@gmail.com>
2024-09-14 22:45:57 +0200

    feat: delete oldest message when reached 100 messages

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -15,20 +15,27 @@ export const messageRouter = router({
     .input(z.object({ text: z.string().max(128), author: z.string().max(16) }))
     .mutation(async ({ ctx, input }) => {
       const { author, text } = input;
-      const createdMessage = await ctx.db.insert(message).values({
+      await ctx.db.insert(message).values({
         author: author,
         text: text,
       });
-      //   const oldestUser = await ctx.db
-      //     .select(message)
-      //     .orderBy(message.createdAt)
-      //     .limit(1);
       const count = await ctx.db
         .select({
           count: sql`count(*)`.as<number>(),
         })
         .from(message);
-      console.log(count);
-      //   await ctx.db.delete(message).where(eq(message.id, oldestUser[0].id));
+      if (count[0]?.count === 101) {
+        const oldestUser = await ctx.db
+          .select()
+          .from(message)
+          .orderBy(message.createdAt)
+          .limit(1)
+          .execute();
+        const oldestUserId = oldestUser[0]?.id as number;
+        await ctx.db
+          .delete(message)
+          .where(eq(message.id, oldestUserId))
+          .execute();
+      }
     }),
 });
