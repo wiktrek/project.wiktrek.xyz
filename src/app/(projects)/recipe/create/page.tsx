df020ee62eca19fdae5ce903f243ec166b909350
wiktor <wiktor.janiszewski10@gmail.com>
2024-12-23 20:24:54 +0100

    feat: react hook form

--- src/app/(projects)/recipe/create/page.tsx
+++ src/app/(projects)/recipe/create/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { NextPage } from "next";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
+import { Input } from "~/app/_components/ui/input";
 import { recipeInputType, recipeValidator } from "~/shared/recipe-validator";
+import { api as trpc } from "~/trpc/react";
+import { auth, currentUser } from "@clerk/nextjs/server";
 const Page: NextPage = async () => {
   // return <h1>I am too lazy to finish this</h1>
   const {
@@ -14,13 +18,77 @@ const Page: NextPage = async () => {
     formState: { errors },
   } = useForm<recipeInputType>({
     resolver: zodResolver(recipeValidator),
-    defaultValues: {},
+    defaultValues: {
+      instructions: [{ name: "", step: "" }],
+    },
   });
+  const mutation = trpc.recipe.createRecipe.useMutation();
+  watch("ingredients");
+  watch("instructions");
+  const { fields, append, remove } = useFieldArray<recipeInputType>({
+    name: "instructions",
+    control,
+  });
+  const user = await currentUser();
   return (
-    <main className="flex items-center justify-center text-center text-2xl">
+    <main className="flex flex-col items-center justify-center text-center text-2xl">
       <div>
         <Link href="/recipe">recipe</Link>
       </div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          mutation.mutate({
+            description: data.description,
+            name: data.name,
+            ingredients: data.ingredients,
+            instructions: data.instructions,
+            email: user!.primaryEmailAddress!.emailAddress,
+            recipe: 0,
+          });
+          reset();
+        })}
+        className="flex flex-col"
+      >
+        <label className="text-sm">name</label>
+        <Input placeholder="name" className="rounded"></Input>
+        <label className="text-sm">description</label>
+        <Input placeholder="description" className="rounded"></Input>
+        <label className="text-sm">Instructions</label>
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <section className="flex pt-1" key={field.id}>
+                <Input
+                  placeholder="Adding flour"
+                  {...register(`instructions.${index}.name`)}
+                ></Input>
+                <Input
+                  placeholder="Add flour..."
+                  {...register(`instructions.${index}.step`, {
+                    required: true,
+                  })}
+                />
+                <button id="" type="button" onClick={() => remove(index)}>
+                  X
+                </button>
+              </section>
+            </div>
+          );
+        })}
+        <button
+          id=""
+          type="button"
+          onClick={() => append({ step: "", name: "" })}
+        >
+          add more instructions
+        </button>
+        {errors.ingredients && (
+          <p id="error">{`${errors.ingredients?.message}`}</p>
+        )}
+        {errors.instructions && (
+          <p id="error">{`${errors.instructions?.message}`}</p>
+        )}
+      </form>
     </main>
   );
 };
