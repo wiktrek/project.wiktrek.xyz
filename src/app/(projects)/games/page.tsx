94ac48bc1b337513f36af303ac6f04510823b67d
wiktor <wiktor.janiszewski10@gmail.com>
2025-01-11 13:52:09 +0100

    feat: created typing/page.tsx

--- src/app/(projects)/games/page.tsx
+++ src/app/(projects)/games/page.tsx
@@ -2,18 +2,22 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="items-center justify-center text-center">
+    <main className="flex flex-col items-center justify-center text-center">
       <p className="text-xl">
         Browser games I made because I wanted to have something to do in school
       </p>
       <ul className="">
-        <li>
-          <Link
-            href="/games/clicker"
-            className="text-xl font-medium text-primary hover:underline"
-          >
+        <li className="text-xl font-medium">
+          <Link href="/games/clicker" className="text-primary hover:underline">
             clicker
           </Link>
+          : simple clicker game
+        </li>
+        <li className="text-xl font-medium">
+          <Link href="/games/aim" className="text-primary hover:underline">
+            aim
+          </Link>
+          : click buttons
         </li>
       </ul>
     </main>
---
+++ src/app/(projects)/games/typing/page.tsx
@@ -0,0 +1,9 @@
+// I will finish this later
+export default function Page() {
+  const texts = ["a quick brown fox jumps over the lazy dog"];
+  return (
+    <div className="text-center text-xl">
+      <p>{texts[0]}</p>
+    </div>
+  );
+}
