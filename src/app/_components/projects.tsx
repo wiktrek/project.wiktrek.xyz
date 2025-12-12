850de12244c3751cd871f4e4d811bcec99acd18d
wiktrek <wiktor.janiszewski10@gmail.com>
2025-12-12 18:23:48 +0100

    feat: tried to make projects more responsive

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -74,7 +74,7 @@ const projects: Project[] = [
 export function Projects() {
   return (
     <section id="projects"  className="flex flex-col w-screen items-center justify-center text-center h-[140vh] md:h-[120vh]">
-      <div className="md:w-[60vw] grid grid-cols-1 md:grid-cols-3 justify-around gap-y-4">
+      <div className="md:w-[60vw] lg:w-[72vw] xl:w-[72vw] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 justify-around gap-y-4">
         {projects.map(p => Project(p))}
       </div>
     </section>
@@ -106,11 +106,11 @@ function Project(p: Project) {
     }
   })
   return (
-    <Link key={p.name} className="border-2 rounded-2xl h-52 w-48 p-2 hover:scale-105 transition-all hover:cursor-crosshair md:w-80 md:h-80 md:p-4" href={p.url}>
+    <Link key={p.name} className="border-2 rounded-2xl h-52 w-48 p-2 hover:scale-105 transition-all hover:cursor-crosshair md:w-60 md:h-64 lg:w-72 lg:h-72 xl:w-80 xl:h-80 md:p-4" href={p.url}>
       <img src={p.image_url}></img>
       <p className="text-left text-lg md:text-xl">{p.name}</p>
       <p className="text-left text-base md:text-lg">{p.description}</p>
-      <div className="text-xs md:text-sm flex space-x-0.5 flex-wrap w-40 md:w-80">{p.tags.map(t => {
+      <div className="text-xs md:text-sm flex space-x-0.5 flex-wrap w-40 md:w-60 lg:w-80">{p.tags.map(t => {
         return <Tag tag={t} key={t}/>
       })}</div>
     </Link>
